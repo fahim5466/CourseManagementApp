7cb0ba115754aeb5fbf6b98f94b4abf4fc826aad
Raihan Kabir Fahim <fahimraihan6@gmail.com>
2025-03-22 14:47:47 +0600

    Added cryptographic hasher which uses the Bcrypt hashing algorithm.

---
+++ Application/Interfaces/ICryptoHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces
+{
+    public interface ICryptoHasher
+    {
+        string SimpleHash(string token);
+        string EnhancedHash(string token);
+        bool Verify(string token, string tokenHash);
+    }
+}
---
+++ Infrastructure/Security/CryptoHasher.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+
+namespace Infrastructure.Security
+{
+    class CryptoHasher : ICryptoHasher
+    {
+        private const int simpleHashWorkFactor = 5;
+        private const int enhancedHashWorkFactor = 13;
+        
+        public string SimpleHash(string token)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(token, simpleHashWorkFactor);
+        }
+
+        public string EnhancedHash(string token)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(token, enhancedHashWorkFactor);
+        }
+
+        public bool Verify(string token, string tokenHash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(token, tokenHash);
+        }
+    }
+}
