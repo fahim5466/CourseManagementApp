b8095eed31a0a2724d429d84ed4183f69a3f5eb6
Raihan Kabir Fahim <fahimraihan6@gmail.com>
2025-03-23 22:20:40 +0600

    Implemented a validation helper to help validate annotated models. This will allow us to move request/model validation to the application layer and also make unit testing easier.

---
+++ Application/Helpers/ValidationHelper.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Helpers
+{
+    public static class ValidationHelper
+    {
+        public class ValidationOutcome(bool isValid, Dictionary<string, List<string>> errors)
+        {
+            public bool IsValid { get; } = isValid;
+            public Dictionary<string, List<string>> Errors { get; } = errors;
+        }
+
+        public static ValidationOutcome Validate<T>(T model) where T : notnull
+        {
+            List<ValidationResult> validationResults = [];
+
+            ValidationContext validationContext = new(model, null, null);
+
+            // Validate the model.
+            bool isValid = Validator.TryValidateObject(
+                model,
+                validationContext,
+                validationResults,
+                validateAllProperties: true
+            );
+
+            // Generate the errors dictionary.
+            Dictionary<string, List<string>> errors = [];
+
+            foreach (ValidationResult validationResult in validationResults)
+            {
+                foreach (string memberName in validationResult.MemberNames)
+                {
+                    if(validationResult.ErrorMessage != null)
+                    {
+                        if(!errors.ContainsKey(memberName))
+                        {
+                            errors[memberName] = [];
+                        }
+                        errors[memberName].Add(validationResult.ErrorMessage);
+                    }
+                }
+            }
+
+            return new ValidationOutcome(isValid, errors);
+        }
+
+        public static ModelStateDictionary ToModelStateDictionary(this Dictionary<string, List<string>> errors)
+        {
+            ModelStateDictionary modelStateDictionary = new();
+
+            foreach(KeyValuePair<string, List<string>> keyValuePair in errors)
+            {
+                string errorEntity = keyValuePair.Key;
+                foreach(string errorMessage in keyValuePair.Value)
+                {
+                    modelStateDictionary.AddModelError(errorEntity, errorMessage);
+                }
+            }
+
+            return modelStateDictionary;
+        }
+    }
+}
