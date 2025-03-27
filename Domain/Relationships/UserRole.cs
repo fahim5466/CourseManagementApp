7718b1b24937f77ab071ead25353bf356a77c198
Raihan Kabir Fahim <fahimraihan6@gmail.com>
2025-03-27 16:19:45 +0600

    Added column ordering to the UserRole class.

--- Domain/Relationships/UserRole.cs
+++ Domain/Relationships/UserRole.cs
@@ -1,11 +1,16 @@
 ﻿using Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Relationships
 {
     public class UserRole
     {
         #region Properties
+        [Key, Column(Order = 0)]
         public Guid UserId { get; set; }
+
+        [Key, Column(Order = 1)]
         public Guid RoleId { get; set; }
         #endregion
 
---
+++ Infrastructure/Migrations/20250327101828_AddColumnOrderingToUserRole.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnOrderingToUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "role_id",
+                table: "user_roles",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "user_id",
+                table: "user_roles",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .Annotation("Relational:ColumnOrder", 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "role_id",
+                table: "user_roles",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "user_id",
+                table: "user_roles",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid")
+                .OldAnnotation("Relational:ColumnOrder", 0);
+        }
+    }
+}
